449b57f8fe4d2ef4f2f337ecb6736b025d2586df
ottis79 <ottaviogentile@gmail.com>
2018-04-03 17:56:38 +0200

    Update

--- SshLog2JSON.js
+++ SshLog2JSON.js
@@ -1,5 +1,6 @@
 SEPARATOR = /^--([0-9a-f]+)-([A-Z])--$/;
-REGULAR_EXP_IPV4 = ^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$;
+REGULAR_EXP_IPV4 = /^--([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))--$/;
+TIME = /^--([A-Za-z]{3}) ([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}) ([A-Za-z0-9\-\ ]+): ([0-9\.\[\]\ ]+) ([A-Za-z0-9\(\)\.\ \-\:]+)--$/;
 
 var SshLog2JSON = function () {
 	var fs = require('fs')
@@ -18,14 +19,10 @@ var SshLog2JSON = function () {
 			rl.on('line', function (line) {
 				line = line.trim();
 				var fsWrite = require('fs');
-				var lineRx = line.match(SEPARATOR);
+				var lineRx = line.match(REGULAR_EXP_IPV4);
 				if(lineRx) {
-				var section_number = lineRx[1];
-				var section_id = lineRx[2];
-				switch(section_id) {
-					
-				} else {
-				
+					var section_number = lineRx[1];
+					var section_id = lineRx[2];
 				}
 			});
 				
