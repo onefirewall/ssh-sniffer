f031c8d6205069053c08e39764bb2477ed648624
ottis79 <ottaviogentile@gmail.com>
2018-04-03 17:57:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var sshLog2JSON = require('./SshLog2JSON.js')
 
-var log2json = new SshLog2JSON()
+var log2json = new sshLog2JSON()
 
 log2json.async("/tmp/sshlogs.log",
 function callback(jsonArray){
