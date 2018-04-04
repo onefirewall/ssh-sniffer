298c0ab0addc859273f02727b7ef346a5bc0235b
OneFirewall <37590172+onefirewall@users.noreply.github.com>
2018-04-04 16:58:57 +0200

    Update SshLog2JSON.js

--- SshLog2JSON.js
+++ SshLog2JSON.js
@@ -1,6 +1,5 @@
-SEPARATOR = /^--([0-9a-f]+)-([A-Z])--$/;
-REGULAR_EXP_IPV4 = /^--([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))--$/;
-TIME = /^--([A-Za-z]{3}) ([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}) ([A-Za-z0-9\-\ ]+): ([0-9\.\[\]\ ]+) ([A-Za-z0-9\(\)\.\ \-\:]+)--$/;
+REGULAR_EXP_IPV4 = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
+TIME = /^--([A-Za-z]{3}) ([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}) ([A-Za-z0-9\-\ ]+): ([0-9\.\[\]\ ]+) ([A-Za-z0-9\(\)\.\ \-\:]+)--$/;
 
 var SshLog2JSON = function () {
     var fs = require('fs'),
@@ -8,25 +7,27 @@ var SshLog2JSON = function () {
         outstream = new (require('stream'))();
 
     this.async = function (filename, callback) {
-            var instream = fs.createReadStream(filename),
-                rl = readline.createInterface(instream, outstream),
-                jsonArray = [],
-                transaction = undefined,
-                section = undefined;
+        var instream = fs.createReadStream(filename),
+             rl = readline.createInterface(instream, outstream),
+             jsonArray = [];
 
-            rl.on('line', function (line) {
-                line = line.trim();
-                var fsWrite = require('fs'),
-                    lineRx = line.match(REGULAR_EXP_IPV4);
-                if (lineRx) {
-                    var section_number = lineRx[1],
-                        section_id = lineRx[2];
-                }
-            });
-                
-            rl.on('close', function (line) {
-                  callback(jsonArray)
-            });
+        rl.on('line', function (line) {
+             line = line.trim();
+             var fsWrite = require('fs');
+             console.log('line'+line);
+             lineRx = line.match(REGULAR_EXP_IPV4);
+             console.log(lineRx);
+             if (lineRx) {
+                 var section_number = lineRx[1],
+                     section_id = lineRx[2];
+                 jsonArray.push(section_number);
+                 jsonArray.push(section_id);
+             }
+        });
+
+        rl.on('close', function (line) {
+            callback(jsonArray)
+        });
     }
- };
- module.exports = SshLog2JSON;
+};         
+module.exports = SshLog2JSON;
