e5b914f9f591799c89ce23d2f21af57b48e07c1e
OneFirewall <37590172+onefirewall@users.noreply.github.com>
2018-04-10 14:48:39 +0200

    Update SshLog2JSON.js

--- SshLog2JSON.js
+++ SshLog2JSON.js
@@ -1,10 +1,9 @@
-var _ = require('underscore');
 REGULAR_EXP_IPV4 = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
 TIME_STAMP_EXP = /([A-Za-z]{3}) ([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/gi;
 
-
 var SshLog2JSON = function () {
-    var fs = require('fs'),
+    var _ = require('underscore'),
+        fs = require('fs'),
         readline = require('readline'),
         outstream = new (require('stream'))();
 
@@ -18,22 +17,21 @@ var SshLog2JSON = function () {
              var fsWrite = require('fs');
              lineIp = line.match(REGULAR_EXP_IPV4);
              var listOfDate = [];
-
              if (lineIp) {
                  var lineTimeStamp = line.match(TIME_STAMP_EXP),
-                     record = {};
-                     var pos = jsonArray.indexOf(lineIp);
-                     if(pos > -1){
-                        console.log('ip already present');
-                        jsonArray[pos].listOfDate.push(lineTimeStamp);
-                    }
-                    else {
-                        console.log('new ip');
-                        record.ip = lineIp;
-                        record.listDate = listOfDate.push(lineTimeStamp);
-                        record.typeInfo = null;
-                        jsonArray.push(record);
-                    }
+                     record = {},
+                 _.map(lineIp, function (item) {
+                     if (!isEmpty(record)) {
+                        if (record.ip.split('.') === lineIp[0].split('.')) {
+                            jsonArray.listOfDate.push(lineTimeStamp);
+                        }
+                     } else {
+                         record.ip = lineIp[0];
+                         record.listDate = lineTimeStamp;
+                         record.typeInfo = null;
+                     }
+                 });
+                 jsonArray.push(record);
              }
         });
 
