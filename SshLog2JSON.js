539b43cb79b731c6610f78275715b244ab7cab2e
OneFirewall <37590172+onefirewall@users.noreply.github.com>
2018-04-13 14:10:28 +0200

    Update SshLog2JSON.js

--- SshLog2JSON.js
+++ SshLog2JSON.js
@@ -1,51 +1,54 @@
 REGULAR_EXP_IPV4 = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
 TIME_STAMP_EXP = /([A-Za-z]{3}) ([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/gi;
-
+var _ = require('underscore');
 var SshLog2JSON = function () {
-    var _ = require('underscore'),
-        fs = require('fs'),
+    var fs = require('fs'),
         readline = require('readline'),
-        outstream = new (require('stream'))(),
-        record = {};
+        outstream = new (require('stream'))();
 
     this.async = function (filename, callback) {
         var instream = fs.createReadStream(filename),
             rl = readline.createInterface(instream, outstream),
-            jsonArray = [];
+            jsonArray = [],
+            record = {};
 
         rl.on('line', function (line) {
              line = line.trim();
              var fsWrite = require('fs'),
-                 lineIp = line.match(REGULAR_EXP_IPV4),
-                 listOfIp = [];
+                 lineIp = line.match(REGULAR_EXP_IPV4);
 
              if (lineIp) {
                  var lineTimeStamp = line.match(TIME_STAMP_EXP);
-                 listOfIp.push(lineIp[0]);
-                 _.map(listOfIp, function (item) {
-                     if (!_.isEmpty(jsonArray[0])) {
-                        _.map(jsonArray[0], function (obj) {
-                            if (obj == item) {
-                                 var arrayDate = record.listOfDate;
-                                 arrayDate.push(lineTimeStamp[0]);
-                                 jsonArray[0].listOfDate = arrayDate;
-                            }
-                        });
-                     } else {
-                         record.ip = item;
-                         record.listOfDate = lineTimeStamp;
-                         record.index = lineIp.index;
-                         record.typeInfo = lineIp.input;
-                         jsonArray.push(record);
-                     } 
-                 });
+                 record.ip = lineIp[0];
+                 record.listOfDate = lineTimeStamp;
+                 record.index = lineIp.index;
+                 record.typeInfo = lineIp.input;
+                 jsonArray.push(record);
              }
         });
 
         rl.on('close', function (line) {
-            callback(jsonArray);
+            callback(SshLog2JSON.validationJsonArray(jsonArray));
         });
     }
-};       
+};
+
+SshLog2JSON.validationJsonArray = function (jsonArray) {
+    var validationJsonArray = [];
+    _.map(jsonArray, function (item) {
+        if (!_.isEmpty(validationJsonArray[0])) {
+            _.map(validationJsonArray, function (obj) {
+                if (obj.ip == item.ip) {
+                    obj.listOfDate.push(item.listOfDate[0]);
+                } else {
+                    validationJsonArray.push(item);
+                }
+            });
+        } else {
+            validationJsonArray.push(item);
+        } 
+    });
+    return validationJsonArray;
+};
 
 module.exports = SshLog2JSON;
