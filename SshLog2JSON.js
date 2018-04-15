c4ca7c6cac3f22db6a6c5db6cd600e572da4c6dc
OneFirewall <37590172+onefirewall@users.noreply.github.com>
2018-04-15 14:03:05 +0200

    Update SshLog2JSON.js

--- SshLog2JSON.js
+++ SshLog2JSON.js
@@ -1,6 +1,6 @@
 REGULAR_EXP_IPV4 = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
 TIME_STAMP_EXP = /([A-Za-z]{3}) ([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/gi;
-var _ = require('underscore');
+
 var SshLog2JSON = function () {
     var fs = require('fs'),
         readline = require('readline'),
@@ -16,7 +16,6 @@ var SshLog2JSON = function () {
              line = line.trim();
              var fsWrite = require('fs'),
                  lineIp = line.match(REGULAR_EXP_IPV4);
-
              if (lineIp) {
                  var lineTimeStamp = line.match(TIME_STAMP_EXP);
                  record.ip = lineIp[0];
@@ -28,27 +27,27 @@ var SshLog2JSON = function () {
         });
 
         rl.on('close', function (line) {
-            callback(SshLog2JSON.validationJsonArray(jsonArray));
+            callback(validationJsonArray(jsonArray));
         });
     }
 };
 
-SshLog2JSON.validationJsonArray = function (jsonArray) {
+function validationJsonArray(jsonArray) {
     var validationJsonArray = [];
-    _.map(jsonArray, function (item) {
+    for (var key in jsonArray) {
         if (!_.isEmpty(validationJsonArray[0])) {
-            _.map(validationJsonArray, function (obj) {
-                if (obj.ip == item.ip) {
-                    obj.listOfDate.push(item.listOfDate[0]);
+            for (var index in validationJsonArray) {
+                if (validationJsonArray[index].ip == jsonArray[key].ip) {
+                    validationJsonArray[index].listOfDate.push(jsonArray[key].listOfDate[0]);
                 } else {
-                    validationJsonArray.push(item);
+                    validationJsonArray.push(jsonArray[key]);
                 }
-            });
+            }
         } else {
-            validationJsonArray.push(item);
-        } 
-    });
+            validationJsonArray.push(jsonArray[key]);
+        }
+    }
     return validationJsonArray;
-};
+}
 
 module.exports = SshLog2JSON;
